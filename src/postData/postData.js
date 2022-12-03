1bb66f0e54336f665b54aa228c7cfa4c2334a941
davidbarco <carlosdavidbarcoh@gmail.com>
2022-12-03 18:55:32 -0500

    prueba

--- src/postData/postData.js
+++ src/postData/postData.js
@@ -3,7 +3,7 @@
             // Opciones por defecto estan marcadas con un *
             const response = await fetch(url, {
               method: 'POST', // *GET, POST, PUT, DELETE, etc.
-              mode: 'same-origin', // no-cors, *cors, same-origin
+              mode: 'no-cors', // no-cors, *cors, same-origin
               cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
               credentials: 'same-origin', // include, *same-origin, omit
               headers: {
